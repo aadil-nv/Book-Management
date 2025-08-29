288a1d33a426e8390fee71508b8703ec3387b8bb
aadil-nv <adilev2000@gmail.com>
2025-08-29 15:04:57 +0530

    customised readme file

--- backend/src/validators/authValidators.ts
+++ backend/src/validators/authValidators.ts
@@ -1,12 +1,25 @@
 import { body } from "express-validator";
 
 export const registerValidation = [
-    body("username").trim().notEmpty().withMessage("Name is required"),
-    body("email").isEmail().withMessage("Invalid email format"),
-    body("password")
-      .isLength({ min: 6 })
-      .withMessage("Password must be at least 6 characters long"),
-  ];
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required"),
+
+  body("email")
+    .isEmail()
+    .withMessage("Invalid email format"),
+
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/\d/)
+    .withMessage("Password must contain at least one digit")
+    .matches(/[@$!%*?&]/)
+    .withMessage("Password must contain at least one special character (@$!%*?&)"),
+];
 
 export const loginValidation = [
   body("email").isEmail().withMessage("Invalid email format"),
